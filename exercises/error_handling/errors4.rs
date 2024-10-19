773a6b09415507b701c91d80b88fc3241b087c0e
poppy-zero <1934144622@qq.com>
2024-10-19 22:47:16 +0800

    update

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -15,27 +15,23 @@ enum CreationError {
 }
 
 impl PositiveNonzeroInteger {
-    fn new(value: i64) -> Result<Self, CreationError> {
-          if value > 0{
-                Ok(PositiveNonzeroInteger(value as u64))   //1.类型转换 2.引入模块或别名：当需要在代码中引入一个模块或者给一个类型起一个别名 }
-          }else if value < 0 {
-                Err(CreationError::Negative)
-          }else{
-                 Err(CreationError::Zero)
-          }
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        if value < 0 {
+            Err(CreationError::Negative)
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        } else {
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
 }
-mod tests {
-    use super::*;
 
-
-    #[test]
-    fn test_creation() {
-        assert!(PositiveNonzeroInteger::new(10).is_ok());
-        assert_eq!(
-            Err(CreationError::Negative),
-            PositiveNonzeroInteger::new(-10)
-        );
-        assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
-    }
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
 }
